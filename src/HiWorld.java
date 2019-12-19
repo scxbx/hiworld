95914203b378b5d95c1c4f64753a32f4a7b485eb
scxbx <30648056+scxbx@users.noreply.github.com>
2019-12-19 17:16:27 +0800

    Update HiWorld.java

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,5 @@
 public class HiWorld {
     public static void main(String[] args) {
-        System.out.println("Hi Tank T-34-85!");
+        System.out.println("Hi Tank T-34-85 T-43 T-44!");
     }
 }
